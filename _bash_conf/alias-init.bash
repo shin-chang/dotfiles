1f267f4000e0bc3eb2e8afc3fdb1975026d78891
matsu ki <m.hisa3153@gmail.com>
2018-01-18 08:35:09 +0900

    add local go alias

--- _bash_conf/alias-init.bash
+++ _bash_conf/alias-init.bash
@@ -107,4 +107,7 @@ alias rs='python manage.py runserver 0.0.0.0:8000'
 # golan
 #--------------------------------------
 alias sg='source ~/.gvm/scripts/gvm'
-
+# local env
+alias gi='gvm use go1.9.1; gvm pkgset use go-tutorial; gvm list; gvm pkgset list'
+alias gcd='cd  ~/go-tutorial/src/github.com/matsu0228/go-cli'
+alias gcb='go run main.go'
